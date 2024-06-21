a9998f663d7b3f38e015f5caf5345ceb58690f31
Hominid2653 <eliascheruiyot9@gmail.com>
2024-06-21 17:49:01 +0300

    Adding login pages but they're not working

---
+++ app/src/main/java/np/com/bimalkafle/quizonline/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package np.com.bimalkafle.quizonline.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
---
+++ app/src/main/java/np/com/bimalkafle/quizonline/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package np.com.bimalkafle.quizonline.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
